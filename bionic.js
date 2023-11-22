2e6773d52b1c3daf2f1ab48c1a55dd1c5e4f5d57
Anton Abrahamsson <abrahamsson37@gmail.com>
2023-11-22 13:21:09 +0100

    Added game page and menu

--- bionic.js
+++ bionic.js
@@ -1,10 +1,20 @@
+// Firstpage
 const playBtn = document.querySelector('.play-btn')
 const startScreen = document.querySelector('#start-display')
 const selectScreen = document.querySelector('#selection')
+const gameScreen = document.querySelector('#game-screen')
 
 playBtn.addEventListener('click', () => {
 	startScreen.classList.add('hide')
 	selectScreen.classList.add('show')
 
+})
+// Selection page
+const easyBtn = document.querySelector('#try-btn')
+const hardBtn = document.querySelector('#hard-btn')
+
+easyBtn.addEventListener('click', ()=> {
+	selectScreen.classList.remove('show')
+	gameScreen.classList.add('show-game')	
 })
 
