ecc42de3c1536a311429e905c34aea8a04579025
wanderingkitty <alinaericson95@gmail.com>
2023-11-23 11:51:40 +0100

    added list function devided in two

--- bionic.js
+++ bionic.js
@@ -7,7 +7,7 @@ const gameScreen = document.querySelector('#game-screen')
 playBtn.addEventListener('click', () => {
 	startScreen.classList.add('hide')
 	selectScreen.classList.add('show')
-
+	
 })
 // Selection page
 const easyBtn = document.querySelector('#try-btn')
@@ -25,23 +25,43 @@ const playerName = document.querySelector('#user-name')
 
 let json = localStorage.getItem(key)
 
-	try {
-		let object = JSON.parse(json)
-		userName.value = object.user
-	}
-	catch {
-		console.log('Nåt gick fel. Försök spara igen.')
-	}
+try {
+	let object = JSON.parse(json)
+	userName.value = object.user
+}
+catch {
+	console.log('Nåt gick fel. Försök spara igen.')
+}
 
-	easyBtn.addEventListener('click', () => {
-		let user = userName.value
-		let data = {
-			user: user
-		}
-		playerName.innerText = userName.value
-		let json = JSON.stringify(data)
+easyBtn.addEventListener('click', () => {
+	let user = userName.value
+	let data = {
+		user: user
+	}
+	playerName.innerText = userName.value
+	let json = JSON.stringify(data)
 	
 	localStorage.setItem(key, json)
 })
 
+import{words} from './svenska-ord.js'
 
+const slumpatOrd = slumpmassigtOrd(words)
+function slumpmassigtOrd(ordLista) {
+	const index = Math.floor(Math.random() * ordLista.length);
+	return ordLista.splice(index, 1)[0];
+}
+
+const lattaOrd = [];
+const svåraOrd = [];
+
+
+for (let i = 0; i < words.length; i++) {
+	const currentWord = words[i];
+	if (currentWord.length <= 10) {
+		lattaOrd.push(currentWord);
+	}else {
+		svåraOrd.push(currentWord);
+	}
+}
+console.log(lattaOrd, svåraOrd);
